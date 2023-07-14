21d6416c1d3de954853f159da31c8918444c571c
Yan Caue <coldecmastercrelan@gmail.com>
2023-07-14 09:46:02 -0300

    novos arquivos

--- Object/object.js
+++ Object/object.js
@@ -8,8 +8,12 @@ let pessoa2 = {
   nome: 'Jesus',
   idade: 2023,
   vivo: false,
+  fala: function() {
+    console.log('Não faltara nada para aqueles que buscam a Jeová');
+  },
 };
 
 console.log(pessoa1.vivo);
 console.log(pessoa2.idade);
 console.log(pessoa1.nome.length);
+pessoa2.fala();
